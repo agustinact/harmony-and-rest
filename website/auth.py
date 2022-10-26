e9aa3a1e48e7bd1bf7bf41cec237f9b0735d55b9
agustinact <113468676+agustinact@users.noreply.github.com>
2022-10-26 11:30:10 -0300

    Tercer commit

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
                 flash('Contraseña incorrecta, intentelo nuevamente.', category='error')
         else:
             flash('Usuario incorrecto, intentelo nuevamente.', category='error')
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -53,9 +53,9 @@ def sign_up():
             new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Cuenta creada', category="success")
             return redirect(url_for('views.home'))
         
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
